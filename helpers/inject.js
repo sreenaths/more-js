ea5f7394d6e8ffe3e1a542f88bbe47fabeebe7d3
Sreenath Somarajapuram <sree@apache.org>
2015-08-14 00:10:55 +0530

    Making the new functions non-enumerable

--- helpers/inject.js
+++ helpers/inject.js
@@ -2,7 +2,11 @@
 
 module.exports = function (target, items) {
   Object.keys(items).forEach(function (key) {
-    items[key]._old_ = items[key]._old_ || target[key],
-    target[key] = items[key];
+    if(target[key]) {
+      items[key]._old_ = target[key]._old_ || target[key];
+    }
+    Object.defineProperty(target, key, {
+      value: items[key]
+    });
   });
 };
