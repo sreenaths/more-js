9c26377ef4ef3cd54dbf266b4036c22fd273669b
Sreenath Somarajapuram <sree@apache.org>
2015-12-11 02:18:39 +0530

    Code cleanup

--- helpers/protoInject.js
+++
@@ -1,8 +0,0 @@
-"use strict"
-
-module.exports = function (target, items) {
-  Object.keys(items).forEach(function (key) {
-    items[key]._old_ = items[key]._old_ || target[key],
-    target[key] = items[key];
-  });
-};
