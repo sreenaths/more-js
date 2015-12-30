52db9b28803d0d8a8e88af03d48caf3fa8e2c937
Sreenath Somarajapuram <sree@apache.org>
2015-12-30 14:01:40 +0530

    Adding more.js that includes all the types

---
+++ more.js
@@ -0,0 +1,8 @@
+"use strict"
+
+module.exports = {
+  Object: require('./types/object'),
+  String: require('./types/string'),
+  Array: require('./types/array'),
+  Function: require('./types/function')
+};
